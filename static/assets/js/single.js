56fbaef8671c7b8ea4bbe28744e233940867d204
hossainemruz <emruz@appscode.com>
2020-07-13 00:10:28 +0600

    Add sidebar + TOC in the reading page

--- static/assets/js/single.js
+++ static/assets/js/single.js
@@ -36,5 +36,44 @@ var isMobile = false, isTablet = false, isLaptop = false;
       }
     }
 
+    // =============== Make TOC Compatible wit Bootstrap Scroll Spy ========
+    // add "navbar" class to the "nav" element
+    let toc = document.getElementById("TableOfContents");
+    toc.classList.add("navbar");
+    // add "nav-pills" class to the "ul" elements
+    let elems = toc.getElementsByTagName("ul");
+    for (let i = 0; i < elems.length; i++) {
+      elems[i].classList.add("nav-pills");
+    }
+    // add "nav-item" class to the "li" elements
+    elems = toc.getElementsByTagName("li");
+    for (let i = 0; i < elems.length; i++) {
+      elems[i].classList.add("nav-item");
+    }
+    // add "nav-link" class to the "a" elements
+    elems = toc.getElementsByTagName("a");
+    for (let i = 0; i < elems.length; i++) {
+      elems[i].classList.add("nav-link");
+    }
   });
 })(jQuery);
+
+// toggle sidebar on click
+function toggleSidebar() {
+  document.getElementById("sidebar-holder").classList.toggle("hide");
+  document.getElementById("sidebar-toggler").classList.toggle("align-left");
+  console.log("Sidebar Toggler clicked...");
+  // if it is mobile device. then scroll to top.
+  // if (isMobile && $("#sidebar").hasClass("hide")) {
+  //   document.body.scrollTop = 0;
+  //   document.documentElement.scrollTop = 0;
+  // }
+}
+
+// toggle table of contents on click
+function toggleTOC() {
+  document.getElementById("toc-holder").classList.toggle("minimize");
+  let btn = document.getElementById("toc-toggler");
+   btn.children[0].classList.toggle("fa-compress");
+   btn.children[0].classList.toggle("fa-stream");
+}
